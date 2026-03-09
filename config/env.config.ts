023ce67f1744a75fef704e0c37f23b3e8664bc8f
appolonist <appolonist@gmail.com>
2026-03-09 13:55:59 +0000

    chore: add dotenv and support for playwright test configuration

---
+++ config/env.config.ts
@@ -0,0 +1,17 @@
+// import dotenv from 'dotenv' throws an error. Replaced it by statement with star because dotenv lib isn't big, so it is safe solution.
+import * as dotenv from 'dotenv';
+
+dotenv.config({ override: true });
+
+function requireEnvVariable(envVariable: string): string {
+  const envVariableValue = process.env[envVariable];
+
+  if (envVariableValue === undefined) {
+    throw new Error(`Environment variable ${envVariable} is not set.`);
+  }
+
+  return envVariableValue;
+}
+
+export const BASE_URL = requireEnvVariable('BASE_URL');
+export const API_KEY = requireEnvVariable('API_KEY');
