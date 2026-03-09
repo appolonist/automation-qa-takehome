1970680aff0e7ceb1052b1203096e319229b92bd
appolonist <appolonist@gmail.com>
2026-03-09 21:50:06 +0000

    fix: remove dotenv info msgs appearing during test execution.

--- config/env.config.ts
+++ config/env.config.ts
@@ -1,7 +1,7 @@
 // import dotenv from 'dotenv' throws an error. Replaced it by statement with star because dotenv lib isn't big, so it is safe solution.
 import * as dotenv from 'dotenv';
 
-dotenv.config({ override: true });
+dotenv.config({ override: true, quiet: true });
 
 function requireEnvVariable(envVariable: string): string {
   const envVariableValue = process.env[envVariable];
